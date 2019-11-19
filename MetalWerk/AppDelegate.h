283da297b115625538d9d8b8e0be054f4535a992
rich@anubis <r.j.henry@gmail.com>
2019-11-19 10:58:12 +0000

    Initial Commit

---
+++ MetalWerk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MetalWerk
+//
+//  Created by Richard Henry on 19/11/2019.
+//  Copyright © 2019 Dogstar Industries Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MetalWerk/GameViewController.h
@@ -0,0 +1,17 @@
+//
+//  GameViewController.h
+//  MetalWerk
+//
+//  Created by Richard Henry on 19/11/2019.
+//  Copyright © 2019 Dogstar Industries Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import "Renderer.h"
+
+// Our iOS view controller
+@interface GameViewController : UIViewController
+
+@end
