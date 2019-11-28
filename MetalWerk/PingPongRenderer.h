c887e7356684c2174bb15ecb2d4479c7b473e1d6
rich@anubis <r.j.henry@gmail.com>
2019-11-28 14:12:44 +0000

    Created a base renderer class and began to split out functionality to a test renderer. Created a shell and source files for the ping pong renderer.

---
+++ MetalWerk/PingPongRenderer.h
@@ -0,0 +1,16 @@
+//
+//  PingPongRenderer.h
+//  MetalWerk
+//
+//  Created by Richard Henry on 19/11/2019.
+//  Copyright © 2019 Dogstar Industries Ltd. All rights reserved.
+//
+
+#import <MetalKit/MetalKit.h>
+
+#import "Renderer.h"
+
+@interface PingPongRenderer : Renderer
+
+@end
+
--- MetalWerk/Renderer.m
+++ MetalWerk/Renderer.m
@@ -11,57 +11,97 @@
 #import "Renderer.h"
 #import "ShaderTypes.h"             // Include header shared between C code here, which executes Metal API commands, and .metal files
 
-static const NSUInteger kUniformBufferMax = 3;
+static const NSUInteger kBufferCountDefault = 3;
 
-@implementation Renderer {
-    
-    dispatch_semaphore_t        frameBufferSemaphore;
-    
-    id <MTLDevice>              device;
-    id <MTLCommandQueue>        commandQueue;
-    
-    id <MTLBuffer>              dynamicUniformBuffer[kUniformBufferMax];
-    id <MTLRenderPipelineState> pipelineState;
-    id <MTLDepthStencilState>   depthState;
-    id <MTLTexture>             colourMap;
-    
-    MTLVertexDescriptor         *vertexDescriptor;
-    uint8_t                     uniformBufferIndex;
-    matrix_float4x4             projectionMatrix;
-    float                       rotation;
-    MTKMesh                     *mesh;
-}
+#pragma mark Renderer base
 
--(nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)view {
+@implementation Renderer
+
+- (nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)view {
     
     if ((self = [super init])) {
         
+        bufferCount = kBufferCountDefault;
+        frameBufferSemaphore = dispatch_semaphore_create(bufferCount);
+        
         device = view.device;
-        frameBufferSemaphore = dispatch_semaphore_create(kUniformBufferMax);
-        [self loadMetalWithView:view];
-        [self loadAssets];
+        commandQueue = [device newCommandQueue]; // Usually, there is only one command queue for each Metal device.
+
+        [self setupView:view];
     }
     
     return self;
 }
 
-- (void)loadMetalWithView:(nonnull MTKView *)view {
+- (void)setupView:(nonnull MTKView *)view {
     
     // Load Metal state objects and initalize renderer dependent view properties
     view.depthStencilPixelFormat = MTLPixelFormatDepth32Float_Stencil8;
     view.colorPixelFormat = MTLPixelFormatBGRA8Unorm_sRGB;
     view.sampleCount = 1;
+}
+
+- (id <MTLCommandBuffer>)createCommandBuffer:(NSString *)name {
     
-    MTLRenderPipelineDescriptor *pipelineStateDescriptor = [[MTLRenderPipelineDescriptor alloc] init];
-    pipelineStateDescriptor.label = @"MyPipeline";
+    dispatch_semaphore_wait(frameBufferSemaphore, DISPATCH_TIME_FOREVER);
+    
+    bufferIndex = (bufferIndex + 1) % bufferCount;
+    
+    id <MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
+    commandBuffer.label = name;
+    
+    __block dispatch_semaphore_t block_sema = frameBufferSemaphore;
+    [commandBuffer addCompletedHandler:^(id<MTLCommandBuffer> buffer) { dispatch_semaphore_signal(block_sema); }];
+    return commandBuffer;
+}
+
+- (void)drawInMTKView:(nonnull MTKView *)view {
+}
+
+- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size {
+    
+    /// Respond to drawable size or orientation changes here
+    
+    float aspect = size.width / size.height;
+    
+    NSLog(@"New aspect ratio = %f", aspect);
+}
+
+@end
+
+#pragma mark - Test Renderer
+
+@implementation TestRenderer {
+    
+    MTLVertexDescriptor         *vertexDescriptor;
+    id <MTLRenderPipelineState> pipelineState;
+    id <MTLDepthStencilState>   depthState;
+
+    // Update time
+    id <MTLBuffer>              dynamicUniformBuffer[kBufferCountDefault]; // TODO: Dynamically allocate this
+    
+    // Asset load time
+    id <MTLTexture>             colourMap;
+    MTKMesh                     *mesh;
+
+    matrix_float4x4             projectionMatrix;
+    float                       rotation;
+}
+
+- (void)setupView:(nonnull MTKView *)view {
+    
+    [super setupView:view];
+    
+    MTLRenderPipelineDescriptor *pipelineStateDescriptor = [MTLRenderPipelineDescriptor new];
+    pipelineStateDescriptor.label = @"Renderer.pipeline";
     pipelineStateDescriptor.sampleCount = view.sampleCount;
     
     pipelineStateDescriptor.colorAttachments[0].pixelFormat = view.colorPixelFormat;
     pipelineStateDescriptor.depthAttachmentPixelFormat = view.depthStencilPixelFormat;
     pipelineStateDescriptor.stencilAttachmentPixelFormat = view.depthStencilPixelFormat;
-    
+
     // Do summat
-    id<MTLLibrary> defaultLibrary = [device newDefaultLibrary];
+    id <MTLLibrary> defaultLibrary = [device newDefaultLibrary];
     pipelineStateDescriptor.vertexFunction = [defaultLibrary newFunctionWithName:@"vertexShader"];
     pipelineStateDescriptor.fragmentFunction = [defaultLibrary newFunctionWithName:@"fragmentShader"];
     
@@ -96,12 +136,12 @@ static const NSUInteger kUniformBufferMax = 3;
     depthStateDesc.depthWriteEnabled = YES;
     depthState = [device newDepthStencilStateWithDescriptor:depthStateDesc];
     
-    for (unsigned i = 0; i < kUniformBufferMax; i++) {
+    for (unsigned i = 0; i < bufferCount; i++) {
         dynamicUniformBuffer[i] = [device newBufferWithLength:sizeof(Uniforms) options:MTLResourceStorageModeShared];
         dynamicUniformBuffer[i].label = [NSString stringWithFormat:@"UniformBuffer %d", i];
     }
     
-    commandQueue = [device newCommandQueue];
+    [self loadAssets];
 }
 
 - (void)loadAssets {
@@ -123,7 +163,7 @@ static const NSUInteger kUniformBufferMax = 3;
     
     if (!mesh || error) NSLog(@"Error creating MetalKit mesh %@", error.localizedDescription);
     
-    MTKTextureLoader* textureLoader = [[MTKTextureLoader alloc] initWithDevice:device];
+    MTKTextureLoader *textureLoader = [[MTKTextureLoader alloc] initWithDevice:device];
     NSDictionary *textureLoaderOptions = @{ MTKTextureLoaderOptionTextureUsage : @(MTLTextureUsageShaderRead), MTKTextureLoaderOptionTextureStorageMode : @(MTLStorageModePrivate) };
     
     colourMap = [textureLoader newTextureWithName:@"ColorMap" scaleFactor:1.0 bundle:nil options:textureLoaderOptions error:&error];
@@ -131,10 +171,10 @@ static const NSUInteger kUniformBufferMax = 3;
     if (!colourMap || error) NSLog(@"Error creating texture %@", error.localizedDescription);
 }
 
-- (void)updateGameState {
+- (void)updateUniforms {
     
     // Update any game state before encoding renderint commands to our drawable
-    Uniforms *uniforms = (Uniforms *)dynamicUniformBuffer[uniformBufferIndex].contents;
+    Uniforms *uniforms = (Uniforms *)dynamicUniformBuffer[bufferIndex].contents;
     
     uniforms->projectionMatrix = projectionMatrix;
     
@@ -150,17 +190,9 @@ static const NSUInteger kUniformBufferMax = 3;
 - (void)drawInMTKView:(nonnull MTKView *)view {
     
     // Per frame updates here
-    dispatch_semaphore_wait(frameBufferSemaphore, DISPATCH_TIME_FOREVER);
+    id <MTLCommandBuffer> commandBuffer = [self createCommandBuffer:@"TestRenderer Command Buffer"];
     
-    uniformBufferIndex = (uniformBufferIndex + 1) % kUniformBufferMax;
-    
-    id <MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];
-    commandBuffer.label = @"MyCommand";
-    
-    __block dispatch_semaphore_t block_sema = frameBufferSemaphore;
-    [commandBuffer addCompletedHandler:^(id<MTLCommandBuffer> buffer) { dispatch_semaphore_signal(block_sema); }];
-    
-    [self updateGameState];
+    [self updateUniforms];
     
     /// Delay getting the currentRenderPassDescriptor until absolutely needed. This avoids
     ///   holding onto the drawable and blocking the display pipeline any longer than necessary
@@ -180,8 +212,8 @@ static const NSUInteger kUniformBufferMax = 3;
         [renderEncoder setRenderPipelineState:pipelineState];
         [renderEncoder setDepthStencilState:depthState];
         
-        [renderEncoder setVertexBuffer:dynamicUniformBuffer[uniformBufferIndex] offset:0 atIndex:BufferIndexUniforms];
-        //[renderEncoder setFragmentBuffer:dynamicUniformBuffer[uniformBufferIndex] offset:0 atIndex:BufferIndexUniforms];
+        [renderEncoder setVertexBuffer:dynamicUniformBuffer[bufferIndex] offset:0 atIndex:BufferIndexUniforms];
+        //[renderEncoder setFragmentBuffer:dynamicUniformBuffer[bufferIndex] offset:0 atIndex:BufferIndexUniforms];
         
         for (unsigned bufferIndex = 0; bufferIndex < mesh.vertexBuffers.count; bufferIndex++) {
             
